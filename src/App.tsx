9b84eee8b737c48965e41157fa4afa7b1bb6ac76
Aryan Gaire <botx711@gmail.com>
2025-05-29 20:56:12 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,6 @@ import "./index.css";
 export default function App() {
   return (
     <BrowserRouter>
-        <div
-        style={{
-          position: "fixed",
-          inset: 0,
-          zIndex: -10,
-          backgroundImage: "url('/circuit-dark.svg')",
-          backgroundRepeat: "repeat",
-          backgroundSize: "auto",
-          backgroundPosition: "top left",
-        }}
-      />
       <LoadingScreen>
         <Routes>
           <Route path="/" element={<Index />} />
