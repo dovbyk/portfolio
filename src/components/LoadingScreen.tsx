d3438a15a5c00341ca2bb75431922624520f55cd
Aryan Gaire <botx711@gmail.com>
2025-05-29 20:43:20 +0530

    Update LoadingScreen.tsx

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -49,7 +49,7 @@ const LoadingScreen: React.FC<LoadingScreenProps> = ({ children }) => {
     <>
       {isLoading && (
         <motion.div
-          className="fixed inset-0 z-50 flex items-center justify-center bg-background"
+          className="fixed inset-0 z-50 flex items-center justify-center "
           initial={{ opacity: 1 }}
           animate={{ opacity: 0 }}
           transition={{ duration: 0.8, ease: "easeInOut", delay: 1 }}
